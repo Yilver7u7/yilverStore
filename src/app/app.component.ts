5cbb2d1941526498cd7a5a7623531830b25f3036
Yilver7u7 <yilversgg@gmail.com>
2024-11-13 10:56:16 -0500

    Testing the API with the pipe json and add common module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductsService } from './api/products.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

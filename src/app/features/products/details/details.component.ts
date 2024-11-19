feda5c4e274fe8ec5f68d2a98a189eecfc9caf6e
Yilver7u7 <yilversgg@gmail.com>
2024-11-19 12:26:33 -0500

    Add the correctly the starts rate, and attach the shadow to img

--- src/app/features/products/details/details.component.ts
+++ src/app/features/products/details/details.component.ts
@@ -1,5 +1,6 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component, inject, input, Input, OnInit, Signal } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ProductsService } from '@api/products.service';
 import { Product } from '@shared/models/product.interface';
 
@@ -12,6 +13,8 @@ import { Product } from '@shared/models/product.interface';
 })
 export default class DetailsComponent implements OnInit{
 
+  starsArray: number[] = new Array(5).fill(0);
+
   //Lo que fue enviado desde nuestra card y obtenemos de este el ID
   // @Input({alias: 'id'}) productId!: number;
 
@@ -21,11 +24,43 @@ export default class DetailsComponent implements OnInit{
   product!: Signal<Product | undefined>;
 
   private readonly productsService = inject(ProductsService);
+  private readonly _sanitizer = inject(DomSanitizer);
 
   ngOnInit(): void {
     this.product = this.productsService.getProductById(this.productId());
   }
 
   onAddToCart(){}
+
+  generateSVG(index: number): SafeHtml {
+    let svgContent = null;
+
+    const rate = this.product()?.rating.rate as number;
+
+    if (index + 1 <= Math.floor(rate)) {
+      svgContent = `<svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+              stroke-width="2" class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+              </path>
+            </svg>`;
+    } else if (index < rate) {
+      svgContent = `<svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
+          <defs>
+            <linearGradient id="partialFillGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset="50%" style="stop-color:currentColor; stop-opacity:1" />
+              <stop offset="50%" style="stop-color:currentColor; stop-opacity:0" />
+            </linearGradient>
+          </defs>
+          <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" fill="url(#partialFillGradient)"></path>
+        </svg>`;
+    } else {
+      svgContent = `<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+              </path>
+            </svg>`;
+    }
+    return this._sanitizer.bypassSecurityTrustHtml(svgContent);
+  }
 
 }

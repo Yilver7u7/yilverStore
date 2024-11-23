ad6a33170beb08eb3f7a2e562f9d86f3c48274a2
Yilver7u7 <yilversgg@gmail.com>
2024-11-23 12:58:35 -0500

    Restore deploy

--- src/app/features/checkout/checkout.service.ts
+++ src/app/features/checkout/checkout.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { environment } from '@envs/environment';
+import { environment } from '../../../environments/environment';
 import { Product } from '@shared/models/product.interface';
 import { loadStripe } from '@stripe/stripe-js';
 import { map } from 'rxjs';
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiURL: 'https://fakestoreapi.com',
+  stripeAPIKey: 'pk_test_51QOLu0BhDXj7Wugy7CcAwCaYRVHDmKiMxdoBFZqi0TWq2nG3bCwEvpEz9RQvT7Nah6If3Jmyt1I6O62PdE6AjgK200QdmHFTke',
+  serverURL: 'http://localhost:4242',
+};
